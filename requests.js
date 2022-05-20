b99b7cf28712b1d9342a95bf824fb95dabf8b521
AndreLarson <dre.larson7@gmail.com>
2022-05-20 14:42:13 -0700

    create smoothie functionality

--- requests.js
+++ requests.js
@@ -22,6 +22,7 @@ function createIngredientList() {
         // create input for list item
         let input = document.createElement('input')
         input.setAttribute('type', 'checkbox')
+        input.setAttribute('id', i)
         // create label for list item
         let label = document.createElement('label')
         label.setAttribute('for', 'checkbox')
@@ -34,6 +35,39 @@ function createIngredientList() {
     }
     // add unordered list to html
     document.getElementById('ingredientList').appendChild(list)
+}
+
+// adds a smoothie to smoothie relation
+function createSmoothie() {
+    // throw some kind of error if name is null
+    let name = document.getElementById('sname').value;
+    let desc = document.getElementById('sdesc').value;
+    let inst = document.getElementById('sinst').value;
+    // open post request to /createSmoothie endpoint
+    http.open("POST", baseUrl + "createSmoothie?" + "name=" + name + "&instructions=" + inst + "&description=" + inst)
+    http.send()
+    http.onreadystatechange = (e) => {
+        // on success add chosen ingredient to uses relation
+        if (http.status === 200 && http.readyState === XMLHttpRequest.DONE) {
+            // retrieve smoothie id from request
+            let s_id = JSON.parse(http.responseText)['smoothieId']
+            // loop through checkboxs and add ingredients to smoothie if box is checked
+            for (let i = 0; i < this.ingredients.length; i++) {
+                let checked = document.getElementById(i).checked
+                if (checked) addIngredient(s_id, this.ingredients[i].ingredient_id, 1, new XMLHttpRequest())
+            }
+        }
+    }
+}
+
+// adds ingredient to smoothie. should only be called within createSmoothie()
+function addIngredient(s_id, i_id, quantity, req) {
+    console.log(s_id, i_id, quantity)
+    req.open("POST", baseUrl + "addIngredient?" + "s_id=" + s_id + "&i_id=" + i_id + "&quantity=" + quantity)
+    req.send()
+    req.onreadystatechange = (e) => {
+        console.log(JSON.parse(req.responseText))
+    }
 }
 
 function getSmoothies() {
