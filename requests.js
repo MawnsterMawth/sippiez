ece1543d544753aebc34714cee4804e4828809af
AndreLarson <dre.larson7@gmail.com>
2022-05-20 12:22:19 -0700

    added unordered list of ingredients from server

--- requests.js
+++ requests.js
@@ -1,12 +1,39 @@
 const http = new XMLHttpRequest()
 const baseUrl = 'https://sippiez.herokuapp.com/'
 
+this.ingredients = null;
+
+// calls a get request to the server to get the all the ingredients
 function getIngredients() {
     http.open("GET", baseUrl + "ingredients")
     http.send()
     http.onreadystatechange = (e) => {
-        console.log(JSON.parse(http.responseText)["ingredients"])
+        this.ingredients = JSON.parse(http.responseText)["ingredients"]
     }
+}
+
+// creates a list of checkable ingredients
+function createIngredientList() {
+    // create unordered list
+    let list = document.createElement('ul')
+    for (let i = 0; i < this.ingredients.length; i++) {
+        // create list item
+        let item = document.createElement('li')
+        // create input for list item
+        let input = document.createElement('input')
+        input.setAttribute('type', 'checkbox')
+        // create label for list item
+        let label = document.createElement('label')
+        label.setAttribute('for', 'checkbox')
+        label.innerHTML = this.ingredients[i].i_name
+        // append input and label to list item
+        item.appendChild(input)
+        item.appendChild(label)
+        // append list item to unordered list
+        list.appendChild(item)
+    }
+    // add unordered list to html
+    document.getElementById('ingredientList').appendChild(list)
 }
 
 function getSmoothies() {
