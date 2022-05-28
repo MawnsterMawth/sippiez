c97f6e4b13ef72101b00a72974456e7ab41ff267
AndreLarson <dre.larson7@gmail.com>
2022-05-27 18:25:32 -0700

    improved create page design

--- create.js
+++ create.js
@@ -20,46 +20,52 @@ function ingredientList() {
     let div = document.getElementById('ingredientListCreate')
     div.setAttribute('class', 'container')
 
+
     for (let i = 0; i < this.ingredients.length; i++) {
-        // create a list item
-        let div_row = document.createElement('div')
-        div_row.setAttribute('class', 'row')
-        div.appendChild(div_row)
+        for (let j = 0; j < 3; j++) {
+            let col = document.getElementById('col' + j)
+            console.log('col' + j)
+            // create a list item
+            let div_row = document.createElement('div')
+            div_row.setAttribute('class', 'row')
+            col.appendChild(div_row)
 
-        // create a checkbox
-        let checkbox = document.createElement('input')
-        checkbox.setAttribute('type', 'checkbox')
-        checkbox.setAttribute('id', "create" + i)
-        checkbox.setAttribute('class', 'form-check-input me-1')
-        let div_col1 = document.createElement('div')
-        div_col1.setAttribute('class', 'col col-sm-2')
-        div_col1.appendChild(checkbox)
-        div_row.appendChild(div_col1)
+            // create a checkbox
+            let checkbox = document.createElement('input')
+            checkbox.setAttribute('type', 'checkbox')
+            checkbox.setAttribute('id', "create" + i)
+            checkbox.setAttribute('class', 'form-check-input')
+            let div_col1 = document.createElement('div')
+            div_col1.setAttribute('class', 'col col-sm-5')
+            div_col1.appendChild(checkbox)
+            div_row.appendChild(div_col1)
 
-        // create a label
-        let label = document.createElement('label')
-        label.setAttribute('for', 'checkbox')
-        label.innerHTML = this.ingredients[i].i_name
-        div_col1.appendChild(label)
+            // create a label
+            let label = document.createElement('label')
+            label.setAttribute('for', 'checkbox')
+            label.innerHTML = this.ingredients[i].i_name
+            div_col1.appendChild(label)
 
-        // create a text input for quantity
-        let quantity = document.createElement('div')
-        quantity.setAttribute('class', 'input-group mb-1 sm-2')
-        let input = document.createElement('input')
-        input.setAttribute('type', 'number')
-        input.setAttribute('class', 'form-control')
-        input.setAttribute('id', 'quantity' + i)
-        input.setAttribute('aria-describedby', 'ounces')
-        quantity.appendChild(input)
-        let span = document.createElement('span')
-        span.setAttribute('class', 'input-group-text')
-        span.setAttribute('id', 'ounces')
-        span.innerHTML = 'ounces'
-        quantity.appendChild(span)
-        let div_col2 = document.createElement('div')
-        div_col2.setAttribute('class', 'col col-sm-2')
-        div_col2.appendChild(quantity)
-        div_row.appendChild(div_col2)
+            // create a text input for quantity
+            let quantity = document.createElement('div')
+            quantity.setAttribute('class', 'input-group')
+            let input = document.createElement('input')
+            input.setAttribute('type', 'number')
+            input.setAttribute('class', 'form-control')
+            input.setAttribute('id', 'quantity' + i)
+            input.setAttribute('aria-describedby', 'ounces')
+            quantity.appendChild(input)
+            let span = document.createElement('span')
+            span.setAttribute('class', 'input-group-text')
+            span.setAttribute('id', 'ounces')
+            span.innerHTML = 'ounces'
+            quantity.appendChild(span)
+            let div_col2 = document.createElement('div')
+            div_col2.setAttribute('class', 'col col-sm-5')
+            div_col2.appendChild(quantity)
+            div_row.appendChild(div_col2)
+            i++
+        }
     }
 }
 
